c29cdeb5eed097e6737fec531e4e4bdb17627583
android8478 <android8478>
2019-09-16 21:52:06 -0300

    refactoring

--- app/src/main/java/br/com/caelum/twittelumappweb/fragment/BuscadorDeTweetsFragment.kt
+++ app/src/main/java/br/com/caelum/twittelumappweb/fragment/BuscadorDeTweetsFragment.kt
@@ -24,8 +24,7 @@ class BuscadorDeTweetsFragment: Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_lista_tweets, container, false)
-        return view
+        return inflater.inflate(R.layout.fragment_lista_tweets, container, false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -51,6 +50,8 @@ class BuscadorDeTweetsFragment: Fragment() {
     private fun filtraTweetPeloTexto(texto: String?): List<Tweet>? {
         val tweets = viewModel.getTweets()
 
-        return tweets.value?.filter {tweet -> tweet.mensagem.contains(texto!!, true) } ?: emptyList()
+        return tweets.value?.filter {
+            it.mensagem.contains(texto!!, ignoreCase = true)
+        }
     }
 }
